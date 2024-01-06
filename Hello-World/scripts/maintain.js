e9cea7ae947b880a5c52336dc75a2e259f9382df
Chikodinaka Ann Anyanwu <chikodiann@gmail.com>
2024-01-06 08:00:00 +0100

    updated prod

--- Hello-World/scripts/maintain.js
+++ Hello-World/scripts/maintain.js
@@ -31,3 +31,12 @@
 //   console.error(error);
 //   process.exitCode = 1;
 // });
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
